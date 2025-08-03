d95867256921bfc6d608e76b5db182adff205ad0
Tuatini <email@tuatini.me>
2025-08-03 18:37:18 +0700

    Add basic structure

--- src/serve/zenoh.rs
+++ src/serve/zenoh.rs
@@ -1,9 +1,17 @@
+struct Zenoh {
+    sent_messages: Vec<String>,
+}
 
-
-pub struct Zenoh {
-    
+impl Zenoh {
+    fn new() -> Zenoh {
+        Zenoh {
+            sent_messages: vec![],
+        }
+    }
 }
 
 impl Messenger for Zenoh {
-    
+    fn send(&self, message: &str) {
+        self.sent_messages.push(String::from(message));
+    }
 }
