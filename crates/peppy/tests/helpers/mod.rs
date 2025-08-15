bf507efae8798e528109f30dc751851cbece80a5
Tuatini Godard <email@tuatini.me>
2025-08-15 20:27:23 +0700

    Fix init

--- crates/peppy/tests/helpers/mod.rs
+++ crates/peppy/tests/helpers/mod.rs
@@ -2,8 +2,9 @@ use peppy::commands::init;
 
 pub fn setup() {
     println!("Setup function called!");
+    let current_dir = std::env::current_dir().expect("Failed to get current directory");
 
-    match init::init() {
+    match init::init(&current_dir) {
         Ok(path) => println!("Initialized peppy.star at: {:?}", path),
         Err(e) => eprintln!("Failed to initialize: {}", e),
     }
