d03f0c20168c80c138b3f5e5c0c7d442ae673994
Tuatini <email@tuatini.me>
2025-08-16 10:34:27 +0700

    Add TODO

--- crates/peppy/src/commands/init.rs
+++ crates/peppy/src/commands/init.rs
@@ -11,11 +11,13 @@ pub fn init(path: &Path) -> Result<PathBuf, std::io::Error> {
 
     // Render the template
     let template = InitStarTemplate {};
-    let default_content = template.render().map_err(|e| {
-        std::io::Error::other(format!("Template error: {}", e))
-    })?;
+    let default_content = template
+        .render()
+        .map_err(|e| std::io::Error::other(format!("Template error: {}", e)))?;
 
     fs::write(&peppy_star_path, default_content)?;
+
+    // TODO: Must also install the systemd service in the OS if it's not already the case"
     Ok(peppy_star_path)
 }
 
