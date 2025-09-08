79515e48fc8e34adc4de78a5ed7e99b51a0c5467
Tuatini Godard <email@tuatini.me>
2025-09-08 11:14:52 +0700

    Add refactoring

--- crates/config-internal/src/lib.rs
+++ crates/config-internal/src/lib.rs
@@ -1,14 +1,10 @@
 mod config;
 mod error;
-mod transformers;
-mod types;
-mod watcher;
+mod watch;
 
 pub mod consts;
 
-pub use config::ConfigTemplateType;
-pub use config::{NodeConfig, NodeConfigCreator, NodeConfigParser};
+pub use config::{ConfigTemplateType, NodeConfig, NodeConfigCreator, NodeConfigParser};
+pub use watch::NodeConfigWatcher;
+
 pub use error::{Error as ConfigError, Result as ConfigResult};
-pub use transformers::NodeConfigWatcher;
-pub use types::FileEvent;
-pub use watcher::{find_peppy_nodes_from_dir, watch_files};
---
+++ crates/config-internal/src/watch.rs
@@ -0,0 +1,6 @@
+mod discovery;
+mod events;
+mod fs;
+mod node_index;
+
+pub use node_index::NodeConfigWatcher;
