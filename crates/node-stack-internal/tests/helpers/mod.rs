1a5ea07656ad44fbb673ebb115c736ba543739f0
Tuatini Godard <email@tuatini.me>
2025-09-30 16:51:31 +0700

    Improve test

--- crates/node-stack-internal/tests/helpers/mod.rs
+++ crates/node-stack-internal/tests/helpers/mod.rs
@@ -2,13 +2,16 @@ use askama::Template;
 use git2::{Repository, Signature};
 use std::{
     fs,
+    io::Write,
     path::{Path, PathBuf},
 };
+use tempfile::NamedTempFile;
 
 #[derive(Template)]
 #[template(path = "root_example_1/peppy.json5.j2")]
 struct RootNodeTemplate1<'a> {
     node_name: &'a str,
+    uvc_camera_github_repo: &'a str,
 }
 
 #[derive(Template)]
@@ -23,17 +26,30 @@ struct WebStreamVideoStreamNodeTemplate<'a> {
     node_name: &'a str,
 }
 
-pub fn create_git_repo(to_path: impl AsRef<Path>) {
-    let repo_path = to_path.as_ref();
-    fs::create_dir_all(repo_path).expect("failed to create repo directory");
-
-    let repo = Repository::init(repo_path).expect("failed to init repository");
-
+pub fn get_root_node() -> PathBuf {
     let root_content = RootNodeTemplate1 {
-        node_name: "root_node",
+        node_name: "peppy_root",
+        uvc_camera_github_repo: "https://github.com/Peppy/nodes.git",
     }
     .render()
     .expect("failed to render root template");
+
+    let mut file = NamedTempFile::new().expect("failed to create temp file for root node");
+    file.write_all(root_content.as_bytes())
+        .expect("failed to write root node content");
+
+    file.into_temp_path()
+        .keep()
+        .expect("failed to persist root node file")
+}
+
+pub fn create_git_repo(to_path: impl AsRef<Path>) -> PathBuf {
+    let base_path = to_path.as_ref();
+    let repo_path = base_path.join("uvc_camera_repo.git");
+    fs::create_dir_all(&repo_path).expect("failed to create repo directory");
+
+    let repo = Repository::init(&repo_path).expect("failed to init repository");
+
     let uvc_content = UvcCameraNodeTemplate {
         node_name: "uvc_camera",
     }
@@ -44,8 +60,6 @@ pub fn create_git_repo(to_path: impl AsRef<Path>) {
     }
     .render()
     .expect("failed to render web template");
-
-    fs::write(repo_path.join("peppy.json5"), root_content).expect("failed to write root node");
 
     let uvc_path = Path::new("nodes/uvc_camera/peppy.json5");
     let web_path = Path::new("nodes/web_video_stream/peppy.json5");
@@ -61,9 +75,6 @@ pub fn create_git_repo(to_path: impl AsRef<Path>) {
     fs::write(repo_path.join(web_path), web_content).expect("failed to write web node");
 
     let mut index = repo.index().expect("failed to open index");
-    index
-        .add_path(Path::new("peppy.json5"))
-        .expect("failed to add root node");
     index.add_path(uvc_path).expect("failed to add uvc node");
     index.add_path(web_path).expect("failed to add web node");
     index.write().expect("failed to write index");
@@ -85,8 +96,10 @@ pub fn create_git_repo(to_path: impl AsRef<Path>) {
     let commit = repo.find_commit(commit_id).expect("failed to find commit");
     repo.tag("0.1.0", commit.as_object(), &signature, "0.1.0", false)
         .expect("failed to create tag");
+
+    repo_path
 }
 
 pub fn setup(current_dir: impl AsRef<Path>) {
-    // TODO: Use current_dir to create a project inside it composed of RootNodeTemplate1/UvcCameraNodeTemplate/WebVideoStreamNodeTemplate1
+    // TODO: Use current_dir to create a project composed of UvcCameraNodeTemplate/WebStreamVideoStreamNodeTemplate
 }
--- crates/node-stack-internal/tests/lib.rs
+++ crates/node-stack-internal/tests/lib.rs
@@ -1,3 +1,4 @@
+use node_stack::LocalNodesMapper;
 use tempfile::TempDir;
 
 #[path = "./helpers/mod.rs"]
@@ -5,8 +6,17 @@ mod helpers;
 
 #[test]
 fn test_create_node_stack() {
+    let root_node = helpers::get_root_node();
     let temp_dir = TempDir::new().unwrap();
-    helpers::create_git_repo(&temp_dir);
-    dbg!(temp_dir.path());
-    let e = 9;
+    let repo_path = helpers::create_git_repo(&temp_dir);
+
+    let mapper = LocalNodesMapper::from_root_config_file(root_node, None).unwrap();
+    let deployment_mapper = mapper.get_local_node_stack().unwrap();
+    let local_node_stack = deployment_mapper.node_stack;
+
+    // Supposed to contain only the root node at this stage (uvc_camera and web_video_stream are pulled from git)
+    assert!(local_node_stack.len() == 1);
+
+    dbg!(&repo_path);
+    let _persisted_path = temp_dir.keep();
 }
