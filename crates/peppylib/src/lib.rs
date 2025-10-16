94f50fabfe90c18d2689dc89dbdedd5585b8bba1
Tuatini Godard <email@tuatini.me>
2025-10-16 15:29:37 +0700

    Add help

--- crates/peppylib/src/lib.rs
+++ crates/peppylib/src/lib.rs
@@ -5,4 +5,5 @@ pub mod checker;
 pub mod messaging;
 
 pub use error::{Error as ControlError, Result as ControlResult};
+pub use messaging::PeppyMessenger;
 pub use node::{setup_node, setup_node_from_config};
