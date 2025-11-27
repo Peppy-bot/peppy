9873ca5c4149b42b4fd24ac5cce8e51c0169e82c
Tuatini Godard <email@tuatini.me>
2025-11-27 08:12:24 +0700

    Add specialized errors

--- crates/generator-internal/templates/peppygen/rust/src/error.rs
+++ crates/generator-internal/templates/peppygen/rust/src/error.rs
@@ -7,8 +7,10 @@ pub type Result<T> = core::result::Result<T, Error>;
 
 #[derive(Debug, Error)]
 pub enum Error {
-    #[error("invalid node name `{node_name}`. Unauthorized characters found")]
-    InvalidNodeName { node_name: String },
+    #[error("invalid node name `{node_name}`: {reason}")]
+    InvalidNodeName { node_name: String, reason: String },
+    #[error("invalid master node name `{node_name}`: {reason}")]
+    InvalidMasterNodeName { node_name: String, reason: String },
     #[error("failed to create messenger for topic `{topic_name}` on {host}:{port}")]
     TopicMessengerConnect {
         topic_name: String,
