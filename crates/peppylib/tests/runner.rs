0e7305352b6fed5df3f054c0115f669a0ade56f1
Tuatini Godard <email@tuatini.me>
2025-12-28 08:44:16 +0700

    Add TODOs

--- crates/peppylib/tests/runner.rs
+++ crates/peppylib/tests/runner.rs
@@ -1,4 +1,5 @@
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
 async fn test_runner() {
+    // TODO: Create fake Parameters struct and pass them into runner::run
     todo!("Finish, test the runtime::runner with the mandatory_services()")
 }
