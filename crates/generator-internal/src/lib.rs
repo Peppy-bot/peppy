2291dba331556cd5deba0986d55da34af49fed89
GODARD Tuatini <email@tuatini.me>
2026-01-12 09:51:02 -0700

    Add subscribed interface code generation to node generate command

--- crates/generator-internal/src/lib.rs
+++ crates/generator-internal/src/lib.rs
@@ -4,8 +4,8 @@ mod generator;
 // Exposes all the generated interfaces
 pub use error::Error as GeneratorError;
 
-pub use generator::generate_lib_for_build_system;
 pub use generator::rust::RustGenerator;
 pub use generator::types::{
     DeploymentInterface, InterfaceVariant, LanguageGenerator, SubscribedActionMessage,
 };
+pub use generator::{generate_lib_for_build_system, generate_lib_for_build_system_with_subscribed};
