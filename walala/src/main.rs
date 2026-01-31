114bd91b0063d6aec23b6d5f247b1c1ecb4b0edd
Tuatini Godard <email@tuatini.me>
2026-01-31 22:39:09 +0700

    Remove walala test project directory

--- walala/src/main.rs
+++
@@ -1,9 +0,0 @@
-use peppygen::{NodeBuilder, Parameters, Result};
-
-fn main() -> Result<()> {
-    NodeBuilder::new().run(|args: Parameters, node_runner| async {
-        let _ = args;
-        let _ = node_runner;
-        Ok(())
-    })
-}
